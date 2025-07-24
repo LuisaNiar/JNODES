6640cc541d90a05310e3ecd653c2ee922b78460e
LuisaNiar <luisaniar@unisabana.edu.co>
2025-07-23 22:43:40 -0500

    Docker Push Github Actions

--- modules/docker_push/main.tf
+++ modules/docker_push/main.tf
@@ -1,8 +1,5 @@
 resource "null_resource" "docker_push" {
-  provisioner "local-exec" {
-    command = <<EOT
-      docker tag ${var.image_name}:latest ${var.docker_user}/${var.image_name}:${var.tag}
-      docker push ${var.docker_user}/${var.image_name}:${var.tag}
-    EOT
+  triggers = {
+    always_run = timestamp()
   }
 }
