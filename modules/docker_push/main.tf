03fdd11a92f0329ae9897de6cfeb1f4d0e103095
LuisaNiar <luisaniar@unisabana.edu.co>
2025-07-24 15:50:56 -0500

    Terraform Variables

--- modules/docker_push/main.tf
+++ modules/docker_push/main.tf
@@ -2,8 +2,4 @@ resource "null_resource" "docker_push" {
   triggers = {
     always_run = timestamp()
   }
-
-  provisioner "local-exec" {
-    command = "docker push ${var.docker_user}/${var.image_name}:${var.tag}"
-  }
 }
